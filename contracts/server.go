1a555ccb641c3e98106c4818205edf25738719fe
zxdstyle <xiangdong.zhu@maitang001.com>
2026-04-22 11:55:31 +0800

    1

--- contracts/server.go
+++ contracts/server.go
@@ -16,6 +16,9 @@ type HandlerFunc func(ctx echo.Context) error
 
 type ResourceHandler interface {
 	List(ctx echo.Context) error
+	BatchList(ctx echo.Context) error
 	Create(ctx echo.Context) error
 	Show(ctx echo.Context) error
+	Update(c echo.Context) error
+	Destroy(c echo.Context) error
 }
--- server/echo.go
+++ server/echo.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/xframe-go/x/contracts"
 	"github.com/xframe-go/x/validate"
 )
 
@@ -17,4 +18,13 @@ func NewEcho() *EchoServer {
 	return &EchoServer{
 		Echo: e,
 	}
+}
+
+func (*EchoServer) Resource(group echo.Group, name string, handler contracts.ResourceHandler) {
+	group.GET(name, handler.List)
+	group.GET(name+"/_batch", handler.BatchList)
+	group.GET(name+"/:id", handler.Show)
+	group.POST(name, handler.Create)
+	group.PUT(name+"/:id", handler.Update)
+	group.DELETE(name+"/:id", handler.Destroy)
 }
